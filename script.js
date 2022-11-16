79b8586a0dd086753853253afb82f229e9b75d1b
Manuel Rivadeneyra <anmariis2002@gmail.com>
2022-11-16 14:26:45 -0500

    primer version nde mi web

---
+++ script.js
@@ -0,0 +1,44 @@
+let menuVisible = false;
+//funcion q oculta o muestra el menu
+function mostrarOcultarMenu() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    } else {
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar() {
+    //oculto el menun una vez q selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+
+
+}
+// funcion q aplica las animaciones de la habilidades
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("java");
+        habilidades[3].classList.add("python");
+        habilidades[4].classList.add("sql");
+        habilidades[5].classList.add("php");
+        habilidades[6].classList.add("comunication");
+        habilidades[7].classList.add("teamwork");
+        habilidades[8].classList.add("creativity");
+        habilidades[9].classList.add("dedication");
+        habilidades[10].classList.add("dicipline");
+
+    }
+}
+//detecto el scrolling para aplicar la aninmacion de la barra de habilidades
+
+window.onscroll = function() {
+    efectoHabilidades();
+}
